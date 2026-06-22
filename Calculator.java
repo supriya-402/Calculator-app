e0b1c709d46b92f6ef2f28cdba4914ce2346ed54
supriya-402 <ernapatiupparisupriya@gmail.com>
2026-06-22 19:41:55 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+Calculator.java
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double b = sc.nextDouble();
+
+        System.out.println("1.Addition");
+        System.out.println("2.Subtraction");
+        System.out.println("3.Multiplication");
+        System.out.println("4.Division");
+
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Result = " + (a + b));
+                break;
+            case 2:
+                System.out.println("Result = " + (a - b));
+                break;
+            case 3:
+                System.out.println("Result = " + (a * b));
+                break;
+            case 4:
+                if (b != 0)
+                    System.out.println("Result = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        sc.close();
+    }
+}
